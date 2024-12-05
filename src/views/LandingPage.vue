5ff67e8fb8cca1c7c141f892290cff29327ff0cd
sovanda_m <vanda.can.read@gmail.com>
2024-12-06 01:09:34 +0700

    Added big banner

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,6 +1,28 @@
 <template>
   <PromotionBar />
   <NavBar />
+  <div class="flex items-center bg-black">
+    <img
+      src="../assets/landPageImg/alt-arrow-left-svgrepo-com.svg"
+      alt="left icon"
+      class="w-[50px] h-[50px]"
+    />
+    <img
+      src="../assets/landPageImg/mechanicalkeyboard1.avif"
+      alt="mechanicalkeyboard1"
+      class="w-1/2"
+    />
+    <img
+      src="../assets/landPageImg/mechanicalkeyboard2.avif"
+      alt="mechanicalkeyboard2"
+      class="w-1/2"
+    />
+    <img
+      src="../assets/landPageImg/alt-arrow-right-svgrepo-com.svg"
+      alt="right icon"
+      class="w-[50px] h-[50px]"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
