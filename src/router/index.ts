33d6d58103df5a8545fd318d920afc520f118308
Koungkea101 <nannkoungkea00@gmail.com>
2025-01-14 16:38:41 +0700

    fix conflict

--- src/router/index.ts
+++ src/router/index.ts
@@ -82,16 +82,7 @@ const router = createRouter({
           name:'OthersPage',
           component: () => import('../views/category/OtherPage.vue'),
         },
-        {
-          path:'Discount',
-          name:'DiscountPage',
-          component: () => import('../views/category/discountPage.vue'),
-        },
-        {
-          path:'Budget',
-          name:'BudgetPage',
-          component: () => import('../views/category/BudgetProductPage.vue'),
-        },
+        
 
       ],
       meta: { requiresAuth: true },
